b6e4356678b77d94fe08d41a1939ecd92b3ebb60
shouchann <shouichi361@gmail.com>
2021-09-12 13:35:00 +0900

    ゲームスタートボタン追加

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-
+document.getElementById("game_start").style.display = "none"
 function check(){
     const input_1 = document.getElementById("input-message");
     const input_value = input_1.value;
@@ -6,6 +6,7 @@ function check(){
     if (input_value == v_1){
         document.getElementById("cl_none").style.display = "none";
         document.getElementById("value").innerHTML = "正解！";
+        document.getElementById("game_start").style.display = "block"
     }else{
         document.getElementById("value").innerHTML = "不正解...";
       
