88ed0adc7b2ea778525c4aa59847f3cff19af03f
shouchann <shouichi361@gmail.com>
2021-09-13 11:33:03 +0900

    0913進捗

--- style.js
+++ style.js
@@ -1,4 +1,14 @@
-document.getElementById("game_start").style.display = "none";
+document.getElementById("s_text_1").onclick = function(){
+    document.getElementById("textbox_1").innerHTML = "見た目は普通の雪だるま…だ"
+}
+document.getElementById("s_text_2").onclick = function(){
+    document.getElementById("textbox_1").innerHTML = "山小屋の外観の割にきちんと清掃がされておりまるで新品のようだ。"
+}
+document.getElementById("s_text_3").onclick = function(){
+    document.getElementById("textbox_1").innerHTML = "箱の上にクロスワードが書いてあった！"
+    document.getElementById("q_go_1").style.display = "block"
+    }
+
 function check(){
     const input_1 = document.getElementById("input-message");
     const input_value = input_1.value;
@@ -23,4 +33,5 @@ function check_1(){
         document.getElementById("value").innerHTML = "不正解...もう一度考えてみよう";
     }
 }
+
 
