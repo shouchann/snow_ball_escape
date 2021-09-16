7928bf78caeb6efbe0b919d6de3c1b2dc9a9d77d
shouchann <shouichi361@gmail.com>
2021-09-17 02:25:12 +0900

    謎手前

--- style.js
+++ style.js
@@ -36,18 +36,103 @@ document.getElementById("bg_9").onclick = function(){
 }
 document.getElementById("bg_10").onclick = function(){
     document.getElementById("bg_10").style.display = "none";
-    document.getElementById("bg_11").style.display = "block";
+    document.getElementById("bg_12").style.display = "block";
 }
-document.getElementById("bg_11").onclick = function(){
+
+document.getElementById("bg_12").onclick = function(){
+    document.getElementById("bg_12").style.display = "none";
+    document.getElementById("bg_13").style.display = "block";
+}
+document.getElementById("bg_13").onclick = function(){
+    document.getElementById("bg_13").style.display = "none";
+    document.getElementById("bg_14").style.display = "block";
+}
+document.getElementById("bg_14").onclick = function(){
+    document.getElementById("bg_14").style.display = "none";
+    document.getElementById("bg_15").style.display = "block";
+}
+document.getElementById("bg_15").onclick = function(){
+    document.getElementById("bg_15").style.display = "none";
+    document.getElementById("bg_16").style.display = "block";
+}
+document.getElementById("bg_16").onclick = function(){
+    document.getElementById("bg_16").style.display = "none";
+    document.getElementById("bg_17").style.display = "block";
+}
+document.getElementById("bg_17").onclick = function(){
+    document.getElementById("bg_17").style.display = "none";
+    document.getElementById("bg_18").style.display = "block";
+}
+document.getElementById("bg_18").onclick = function(){
+    document.getElementById("bg_18").style.display = "none";
+    document.getElementById("bg_19").style.display = "block";
+}
+document.getElementById("bg_19").onclick = function(){
+    document.getElementById("bg_19").style.display = "none";
+    document.getElementById("bg_20").style.display = "block";
+}
+document.getElementById("bg_20").onclick = function(){
+    document.getElementById("bg_20").style.display = "none";
+    document.getElementById("bg_21").style.display = "block";
+}
+document.getElementById("bg_21").onclick = function(){
+    document.getElementById("bg_21").style.display = "none";
+    document.getElementById("bg_22").style.display = "block";
+}
+document.getElementById("bg_22").onclick = function(){
+    document.getElementById("bg_22").style.display = "none";
+    document.getElementById("bg_23").style.display = "block";
+}
+document.getElementById("bg_23").onclick = function(){
+    document.getElementById("bg_23").style.display = "none";
+    document.getElementById("bg_24").style.display = "block";
+}
+document.getElementById("bg_24").onclick = function(){
+    document.getElementById("bg_24").style.display = "none";
+    document.getElementById("bg_25").style.display = "block";
+}
+document.getElementById("bg_25").onclick = function(){
+    document.getElementById("bg_25").style.display = "none";
+    document.getElementById("bg_26").style.display = "block";
+}
+document.getElementById("bg_26").onclick = function(){
+    document.getElementById("bg_26").style.display = "none";
+    document.getElementById("bg_27").style.display = "block";
+}
+document.getElementById("bg_27").onclick = function(){
+    document.getElementById("bg_27").style.display = "none";
+    document.getElementById("bg_28").style.display = "block";
+}
+document.getElementById("bg_28").onclick = function(){
+    document.getElementById("bg_28").style.display = "none";
+    document.getElementById("bg_30").style.display = "block";
     document.getElementById("s_l").style.display = "flex";
 }
 document.getElementById("friend").onclick = function(){
-    document.getElementById("").style.display = "block";
+    document.getElementById("bg_31").style.display = "block";
+    document.getElementById("s_l").style.display = "none";
+}
+document.getElementById("bg_31").onclick = function(){
+    document.getElementById("bg_31").style.display = "none";
+    document.getElementById("bg_33").style.display = "block";
+}
+document.getElementById("bg_33").onclick = function(){
+    document.getElementById("bg_33").style.display = "none";
+    document.getElementById("bg_30").style.display = "block";
+    document.getElementById("s_l").style.display = "flex";
 }
 document.getElementById("sofa").onclick = function(){
-    document.getElementById("s_l").style.display = "flex";
+    document.getElementById("bg_32").style.display = "block";
+    document.getElementById("s_l").style.display = "none";
+}
+document.getElementById("bg_32").onclick = function(){
+    document.getElementById("bg_32").style.display = "none";
+    document.getElementById("bg_33").style.display = "block";
 }
 document.getElementById("mis_box").onclick = function(){
-    document.getElementById("s_l").style.display = "flex";
+    document.getElementById("bg_29").style.display = "block";
+    document.getElementById("bg_30").style.display = "none";
+    document.getElementById("s_l").style.display = "none";
 }
+
 
